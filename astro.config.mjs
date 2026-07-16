f0527ebba1e6e8fa1c707c8ede15872cb1c528eb
annans-ivresse <Ivresse.ai@gmail.com>
2026-07-16 22:15:46 +0800

    chore: restore astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ export default defineConfig({
   // 改成你的 GitHub 用户名
   site: 'https://nowornever17.github.io',
   // 如果仓库名不是 username.github.io，需要设置 base
-  // base: '/nowornever17-blog',
+  // base: '/blog/',
   integrations: [mdx()],
 });
