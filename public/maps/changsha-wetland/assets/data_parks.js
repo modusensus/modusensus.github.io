66e39bb68b1e5447950bb027fa8c4d64b65e3f04
modusensus <guanqishi26@gmail.com>
2026-09-03 08:16:26 +0800

    Wetland: add accessibility research on Changsha wetland parks
    Publish the FIELD-01 report (Changsha wetland park accessibility via isochrone analysis) with the GIS method notes, plus the interactive Leaflet map and coverage charts used in the article.

---
+++ public/maps/changsha-wetland/assets/data_parks.js
@@ -0,0 +1,14 @@
+var PARKS = {
+"type": "FeatureCollection",
+"name": "park_pts",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "千龙湖国家湿地公园" }, "geometry": { "type": "Point", "coordinates": [ 112.700583397886945, 28.417662875523114 ] } },
+{ "type": "Feature", "properties": { "name": "大泽湖近自然湿地公园" }, "geometry": { "type": "Point", "coordinates": [ 112.873371314808111, 28.32037565342123 ] } },
+{ "type": "Feature", "properties": { "name": "松雅湖湿地公园" }, "geometry": { "type": "Point", "coordinates": [ 113.089736078403263, 28.274249969869381 ] } },
+{ "type": "Feature", "properties": { "name": "洋湖湿地公园" }, "geometry": { "type": "Point", "coordinates": [ 112.915978871339377, 28.124578034543575 ] } },
+{ "type": "Feature", "properties": { "name": "浏阳河国家湿地公园" }, "geometry": { "type": "Point", "coordinates": [ 113.771535870609341, 28.189207896055013 ] } },
+{ "type": "Feature", "properties": { "name": "金洲湖国家湿地公园" }, "geometry": { "type": "Point", "coordinates": [ 112.55993280144483, 28.248108318696865 ] } }
+]
+}
+;
